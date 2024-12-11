9e1ffbb3e2a76e4d986ba5eb05e79c653e645907
t9daNywe2h6WNfQryGtS <etienne.chaize@enfrasys.fr>
2024-12-11 14:27:08 +0100

    refacto maven

---
+++ src/main/java/fr/sdis/emergencymicroservice/EmergencyMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.sdis.emergencymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/sdis/emergencymicroservice/EmergencyMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sdis.emergencymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
